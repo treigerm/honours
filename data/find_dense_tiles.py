90aa1d54181fb445cb3e71285f2cf0ef20daf28d
Tim Reichelt <treichelt6@gmail.com>
2018-10-29 16:04:19 +0000

    Add script to find non-white density of tiles

---
+++ data/find_dense_tiles.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import openslide
+import numpy as np
+import argparse
+
+SERIES = 0
+
+def non_white_density(image):
+    """
+    Args:
+        image: np.array (tile_size, tile_size, channels)
+    """
+    c1_dens = image[:,:,0] < 200
+    c2_dens = image[:,:,1] < 200
+    c3_dens = image[:,:,2] < 200
+    c1_c2_dens = np.logical_and(c1_dens, c2_dens)
+    pix_dens = np.logical_and(c1_c2_dens, c3_dens)
+    return np.sum(pix_dens) / float(pix_dens.size)
+
+def main(infile, tilesize):
+    slide = openslide.OpenSlide(infile)
+    (max_x, max_y) = slide.dimensions
+    cur_x, cur_y = 0, 0
+    
+    while True:
+        if cur_y + tilesize > max_y:
+            break
+
+        if cur_x + tilesize > max_x:
+            cur_x = 0
+            cur_y += tilesize
+            continue
+        
+        tile = slide.read_region((cur_x, cur_y), SERIES, (tilesize, tilesize))
+        density = non_white_density(np.array(tile))
+        print("{}\t{}\t{}".format(cur_x, cur_y, density))
+        cur_x += tilesize
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--infile", help="Name of the input file.")
+    parser.add_argument("--tilesize", help="Size of the tiles. All tiles are square.",
+                        type=int)
+    args = parser.parse_args()
+    main(**vars(args))
