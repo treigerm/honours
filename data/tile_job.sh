70d63c90439f4c422b412ec616319421d6ac6c9d
Tim Reichelt <treichelt6@gmail.com>
2018-12-23 16:20:59 +0100

    Add datadir as cli argument

--- data/tile_job.sh
+++ data/tile_job.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -pe sharedmem 8
+#$ -pe sharedmem 16
 #$ -cwd
+
+# Script to submit a tiling job to the Eddie job scheduler.
+# The script takes a directory which contains directories of .svs slides and tiles 
+# the .svs slides in parallel
+# Usage (on eddie):
+# $ qsub tile_job.sh /dir/to/data
 
 set -e
 
@@ -14,8 +20,8 @@ source activate honours
 
 SCRIPTSDIR="/home/s1547426/dev/honours/data"
 
-DATADIR="/home/s1547426/data/tcga_gbm"
-NUM_CORES=8
+DATADIR="$1"
+NUM_CORES=16
 TILESIZE=1000
 
 find $DATADIR -mindepth 1 -maxdepth 1 -type d | \
