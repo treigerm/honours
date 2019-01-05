3094564bb5d0236605496fd62d3fb3bba0c8db22
Tim Reichelt <treichelt6@gmail.com>
2019-01-05 15:55:22 +0000

    Add tensorboard support

--- run_exp.py
+++ run_exp.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import torch
+import os
+import tensorboardX
+
 from data.TCGAGBMDataset import TCGAGBMDataset, ToTensor
 from data.dataset import CrossValDataset
 from models.CAE import CAE
@@ -15,25 +18,29 @@ DEVICE = "cpu"
 DATA_DIR = "/Users/Tim/data/tcga_luad/gdc_download_20181013_213421.982108"
 DATA_CSV = "/Users/Tim/data/tcga_luad/gdc_download_20181013_213421.982108/tile_locations.csv"
 
+TENSORBOARD_DIR = "/Users/Tim/dev/cw/honours/tensorboard"
+LOGGING_DIR = "/Users/Tim/dev/cw/honours/logs"
+
 TRAIN_SIZE = 0.7
 VAL_SIZE = 0.2
 
 def test(model, loss_fn, test_loader):
     model.eval()
     test_loss = 0
-    num_batches = 0
     with torch.no_grad():
         for batch in test_loader:
             slides = batch["slide"]
             test_loss += loss_fn(model(slides), slides)
-            num_batches += 1
     
-    test_loss /= num_batches
-    print("Average loss: {:.4f}".format(test_loss))
+    return test_loss / len(test_loader)
 
 
 def main(config):
     device = DEVICE
+
+    writer = tensorboardX.SummaryWriter(os.path.join(TENSORBOARD_DIR, 
+                                                     config["exp_name"]))
+
     dataset = CrossValDataset(DATA_CSV, DATA_DIR, TRAIN_SIZE, VAL_SIZE, 
                               transform=ToTensor(device))
     train_loader = torch.utils.data.DataLoader(
@@ -48,24 +55,34 @@ def main(config):
                                  weight_decay=config["weight_decay"])
     loss_fn = torch.nn.MSELoss()
     for i_epoch in range(config["num_epochs"]):
+        train_loss = 0
         for i_batch, batch in enumerate(train_loader):
             slides = batch["slide"]
-            print(slides[0, :, 0, 0])
 
             model.train()
             optimizer.zero_grad()
 
             loss = loss_fn(model(slides), slides)
+            writer.add_scalar("data/batch_loss", loss, i_batch)
+            train_loss += loss
             loss.backward()
             optimizer.step()
-            if i_batch == 4:
-                break
-        
+
         print("Epoch {}".format(i_epoch))
-        test(model, loss_fn, val_loader)
+
+        train_loss /= len(train_loader)
+        test_loss = test(model, loss_fn, val_loader)
+        writer.add_scalar("data/loss", {"train_loss": train_loss, 
+                                        "test_loss": test_loss}, i_epoch)
+        
+        if i_epoch == 1:
+            break
+    
+    writer.export_scalars_to_json(os.path.join(LOGGING_DIR, config["exp_name"]))
 
 if __name__ == "__main__":
     config = {
+        "exp_name": "test_exp",
         "batch_size": 4,
         "test_batch_size": 100,
         "learning_rate": 5e-3,
