e0e9f7857c93acfb3765f08545038c75c8f84cd3
Tim Reichelt <treichelt6@gmail.com>
2019-01-07 17:32:17 +0000

    Allow for subdirectories in survival/non-survival directories

--- preprocessing/create_csv.py
+++ preprocessing/create_csv.py
@@ -7,20 +7,27 @@ import argparse
 
 FIELDNAMES = ["relative_path", "label", "patientID"]
 
-def find_tiff_files(root_dir, sub_dir):
+def find_tiff_files(root_dir, sub_dir, image_dir=None):
     # TODO: Describe directory structure.
-    d = os.path.join(root_dir, sub_dir, "write_access_data_images")
+    d = os.path.join(root_dir, sub_dir)
     os.chdir(d)
     # Find all .tiff tiles.
-    return glob.glob("*/*.tiff")
+    if image_dir is not None:
+        return glob.glob("{}/*/*.tiff".format(image_dir))
+    else:
+        return glob.glob("*/*.tiff")
 
-def main(root_dir, csv_filename, survival_dir=None, non_survival_dir=None):
+def main(root_dir, 
+         csv_filename, 
+         survival_dir=None, 
+         non_survival_dir=None,
+         image_dir=None):
     with open(csv_filename, "w") as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=FIELDNAMES)
         writer.writeheader()
 
         for label, sub_dir in [(0, non_survival_dir), (1, survival_dir)]:
-            for file_path in find_tiff_files(root_dir, sub_dir):
+            for file_path in find_tiff_files(root_dir, sub_dir, image_dir):
                 row = {
                     "relative_path": file_path,
                     "label": label,
@@ -35,5 +42,6 @@ if __name__ == "__main__":
     parser.add_argument("--csv-filename")
     parser.add_argument("--survival-dir")
     parser.add_argument("--non-survival-dir")
+    parser.add_argument("--image-dir")
     args = parser.parse_args()
     main(**vars(args))
