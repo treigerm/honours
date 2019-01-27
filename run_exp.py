e827c9380851d41682035d23786804f1d6c5e7a1
Tim Reichelt <treichelt6@gmail.com>
2019-01-27 11:01:18 +0000

    Minor improvements on training script

--- run_exp.py
+++ run_exp.py
@@ -6,23 +6,31 @@ import torchvision
 import tensorboardX
 import argparse
 import yaml
+import pickle
+import time
 
 from data.TCGAGBMDataset import TCGAGBMDataset, ToTensor
 from data.dataset import CrossValDataset
 from models.cae import CAE, TestCAE
 from models.factory import get_model
-from utils.logging import make_exp_dir, save_checkpoint
+from utils.logging import make_exp_dir, save_checkpoint, AverageMeter
 
+def save_metrics(metrics, exp_dir):
+    out_file = os.path.join(exp_dir, "metrics.pickle")
+    with open(out_file, "wb+") as f:
+        pickle.dump(metrics, f)
 
 def test(model, device, loss_fn, test_loader):
     model.eval()
     test_loss = 0
+    batches = 0
     with torch.no_grad():
         for batch in test_loader:
             slides = batch["slide"].to(device)
             test_loss += loss_fn(model(slides), slides)
+            batches += 1
     
-    return test_loss / len(test_loader)
+    return test_loss / batches
 
 
 def main(config, exp_dir):
@@ -58,31 +66,47 @@ def main(config, exp_dir):
     model = get_model(config["model_name"]).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=config["learning_rate"],
                                  weight_decay=config["weight_decay"])
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min")
     loss_fn = torch.nn.MSELoss()
 
+    metrics = {"val_loss": [], "train_loss": []}
+    batch_time = AverageMeter()
+    train_loss = AverageMeter()
     best_val_loss = float("inf")
+
     i_episode = 1
     keep_training = True
     while keep_training:
         for batch in train_loader:
+            start = time.time()
             slides = batch["slide"].to(device)
 
             model.train()
             optimizer.zero_grad()
 
             loss = loss_fn(model(slides), slides)
+            train_loss.update(loss)
+            metrics["train_loss"].append(train_loss.val)
+
             loss.backward()
             optimizer.step()
+            batch_time.update(time.time() - start)
 
             if i_episode % config["eval_steps"] == 0:
                 val_loss = test(model, device, loss_fn, val_loader)
+                scheduler.step(val_loss)
 
-                print("Episode {}\ttrain loss: {:.4e} val loss: {:.4e}".format(
-                    i_episode, loss, val_loss
-                ))
+                print("Episode {0}\t"
+                      "Time {batch_time.val:.3f} ({batch_time.avg:.3f}) "
+                      "Train loss {train_loss.val:.4e} ({train_loss.avg:.4e}) "
+                      "Val loss {1:.4e}".format(
+                      i_episode, val_loss, batch_time=batch_time, 
+                      train_loss=train_loss))
+
                 writer.add_scalars("data/losses", {"val_loss": val_loss,
-                                                   "train_loss": loss}, i_episode)
-                writer.export_scalars_to_json(os.path.join(exp_dir, "metrics.json"))
+                                                   "train_loss": train_loss.val}, i_episode)
+                metrics["val_loss"].append(val_loss)
+                save_metrics(metrics, exp_dir)
 
                 is_best = val_loss < best_val_loss
                 best_val_loss = val_loss if is_best else best_val_loss
@@ -94,7 +118,7 @@ def main(config, exp_dir):
                     "optimizer": optimizer.state_dict()
                 }, is_best, path=exp_dir)
 
-            if i_episode == config["num_episodes"]:
+            if i_episode >= config["num_episodes"]:
                 keep_training = False
                 break
 
