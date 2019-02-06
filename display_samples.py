d3c300c5a80a8d659d055a6503c370629b1b56f0
Tim Reichelt <treichelt6@gmail.com>
2019-02-06 15:27:37 +0000

    Make script able to show samples without reconstruction

--- display_samples.py
+++ display_samples.py
@@ -16,35 +16,43 @@ from utils.logging import load_checkpoint
 
 RANDOM_SEED = 42
 
-def save_images(images, out_file=None):
+def save_images(images, out_file=None, have_reconstructed=False):
     fig = plt.figure()
 
-    rows = len(images) / 2
-    cols = 2
+    if have_reconstructed:
+        rows = len(images) / 2
+        cols = 2
+    else:
+        cols = len(images)
+        rows = 1
     for i in range(len(images)):
         ax = plt.subplot(rows, cols, i + 1)
         #plt.tight_layout()
-        if i == 0:
-            ax.set_title("Original")
-        elif i == 1:
-            ax.set_title("Reconstructed")
+        if have_reconstructed:
+            if i == 0:
+                ax.set_title("Original")
+            elif i == 1:
+                ax.set_title("Reconstructed")
         ax.axis("off")
         plt.imshow(images[i])
     plt.savefig(out_file)
 
 
-def main(root_dir, data_csv, checkpoint_path, use_gpu, out_file):
+def main(root_dir, data_csv, crop_size, checkpoint_path, use_gpu, out_file):
     torch.manual_seed(RANDOM_SEED)
     np.random.seed(RANDOM_SEED)
     random.seed(RANDOM_SEED)
 
-    device = torch.device("cuda" if use_gpu else "cpu")
-    model, _ = load_checkpoint(checkpoint_path, device, get_model)
-    model.eval()
+    reconstruct_images = checkpoint_path is not None and use_gpu is not None
+    if reconstruct_images:
+        device = torch.device("cuda" if use_gpu else "cpu")
+        model, _ = load_checkpoint(checkpoint_path, device, get_model)
+        model.eval()
 
+    sample_transform = ToTensor(device) if reconstruct_images else None
     dataset = CrossValDataset(
         data_csv, root_dir,
-        image_transform=torchvision.transforms.RandomCrop(128),
+        image_transform=torchvision.transforms.RandomCrop(crop_size),
         sample_transform=ToTensor(device)
     )
 
@@ -54,13 +62,16 @@ def main(root_dir, data_csv, checkpoint_path, use_gpu, out_file):
 
     with torch.no_grad():
         for sample in data_loader:
-            images = []
-            im_ae = model(sample["slide"].to(device))
-            for i in range(len(sample["slide"])):
-                img = sample["slide"][i]
-                images.append(torchvision.transforms.functional.to_pil_image(img))
-                images.append(torchvision.transforms.functional.to_pil_image(im_ae[i]))
-            save_images(images, out_file)
+            if reconstruct_images:
+                images = []
+                im_ae = model(sample["slide"].to(device))
+                for i in range(len(sample["slide"])):
+                    img = sample["slide"][i]
+                    images.append(torchvision.transforms.functional.to_pil_image(img))
+                    images.append(torchvision.transforms.functional.to_pil_image(im_ae[i]))
+                save_images(images, out_file, have_reconstructed=True)
+            else:
+                save_images(sample["slide"], out_file, have_reconstructed=False)
             break
 
 if __name__ == "__main__":
@@ -68,6 +79,7 @@ if __name__ == "__main__":
     parser.add_argument("--checkpoint-path", type=str)
     parser.add_argument("--root-dir", type=str)
     parser.add_argument("--data-csv", type=str)
+    parser.add_argument("--crop-size", type=int, default=128)
     parser.add_argument("--use-gpu", action="store_true")
     parser.add_argument("--out-file", default="samples.png")
     args = parser.parse_args()
