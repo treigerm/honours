4a8f467b152782c8ca27b7cfa4c7b091d68da37b
Tim Reichelt <treichelt6@gmail.com>
2019-02-06 18:10:00 +0000

    Add Dataset with new transformations and appropriate sampler

---
+++ data/patch_wise.py
@@ -0,0 +1,72 @@
+import os
+import torch
+import numpy as np
+
+from PIL import Image, ImageEnhance
+from torch.utils.data import Dataset
+
+IMAGE_SIZE = (1000, 1000)
+PATCH_SIZE = 128
+STRIDE = PATCH_SIZE
+
+class PatchWiseDataset(Dataset):
+    """
+    Based on: https://github.com/ImagingLab/ICIAR2018/blob/master/src/datasets.py
+    """
+
+    def __init__(self, data_frame, root_dir, 
+                image_transform=None, 
+                sample_transform=None,
+                rotate=True,
+                flip=True,
+                enhance=True):
+        self.slides_frame = data_frame
+        self.root_dir = root_dir
+        self.image_transform = image_transform
+        self.sample_transform = sample_transform
+
+        wp = int((IMAGE_SIZE[0] - PATCH_SIZE) / STRIDE + 1)
+        hp = int((IMAGE_SIZE[1] - PATCH_SIZE) / STRIDE + 1)
+
+        self.shape = (len(self.slides_frame), wp, hp, 
+            (4 if rotate else 1), (2 if flip else 1), (2 if enhance else 1))
+    
+    def __getitem__(self, index):
+        idx, xpatch, ypatch, rotation, flip, enhance = np.unravel_index(index, self.shape)
+
+        relative_path = self.slides_frame.iloc[idx, 0]
+        slide_path = os.path.join(self.root_dir, relative_path)
+        slide = Image.open(slide_path).convert("RGB")
+        patch = slide.crop((
+            xpatch * STRIDE, # left
+            ypatch * STRIDE, # up
+            xpatch * STRIDE + PATCH_SIZE, # right
+            ypatch * STRIDE + PATCH_SIZE # down
+        ))
+
+        if rotation != 0:
+            patch = patch.rotate(rotation * 90)
+
+        if flip != 0:
+            patch = patch.transpose(Image.FLIP_LEFT_RIGHT)
+
+        if enhance != 0:
+            factors = np.random.uniform(.5, 1.5, 3)
+            patch = ImageEnhance.Color(patch).enhance(factors[0])
+            patch = ImageEnhance.Contrast(patch).enhance(factors[1])
+            patch = ImageEnhance.Brightness(patch).enhance(factors[2])
+        
+        sample = {
+            "slide": patch,
+            "label": self.slides_frame.iloc[idx, 1],
+            "case_id": self.slides_frame.iloc[idx, 2],
+            "relative_path": relative_path
+        }
+
+        if self.sample_transform:
+            sample = self.sample_transform(sample)
+        
+        return sample
+    
+    def __len__(self):
+        return np.prod(self.shape)
---
+++ data/samplers.py
@@ -0,0 +1,63 @@
+import torch
+import numpy as np
+import random
+
+CASE_INDEX = 2 # Column index of case ID in metadata table.
+
+def save_pop(l, k):
+    """Pops min(len(l), k) elements from l.
+    """
+    samples = []
+    while len(l) > 0 and len(samples) < k:
+        samples.append(l.pop(random.randrange(len(l))))
+    return samples
+
+def make_batch_sampler(data_source, 
+                       slides_frame, 
+                       cases_per_batch, 
+                       patches_per_case, 
+                       drop_last=False):
+    case_sampler = CaseSampler(
+        data_source, slides_frame, cases_per_batch, patches_per_case)
+    return torch.utils.data.BatchSampler(
+        case_sampler, case_sampler.batch_size, drop_last)
+
+class CaseSampler(torch.utils.data.Sampler):
+    """Sampler for the PatchWiseDataset. 
+    
+    It creates an iterator that can be used with a torch.utils.data.BatchSampler
+    object with a specific batch size. The resulting batches will each have 
+    cases_per_batch cases and for each cases patches_per_case patches."""
+
+    def __init__(self, data_source, slides_frame, cases_per_batch, patches_per_case):
+        self.data_source = data_source
+        self.slides_frame = slides_frame
+        self.cases_per_batch = cases_per_batch
+        self.patches_per_case = patches_per_case
+        self.batch_size = cases_per_batch * patches_per_case
+    
+    def __iter__(self):
+        # Create a dictionary {case_id: [index]} so the indexes are 
+        # grouped by case ID.
+        cases = dict()
+        for i in range(len(self.data_source)):
+            idx, _, _, _, _, _ = np.unravel_index(i, self.data_source.shape)
+            case_id = self.slides_frame.iloc[idx, CASE_INDEX]
+            if case_id in cases:
+                cases[case_id].append(i)
+            else:
+                cases[case_id] = [i]
+        
+        ix_samples = []
+        while len(cases) > 0:
+            num_cases = min(self.cases_per_batch, len(cases))
+            batch_cases = random.sample(cases.keys(), num_cases)
+            for c in batch_cases:
+                ix_samples += save_pop(cases[c], self.patches_per_case)
+                if len(cases[c]) == 0:
+                    cases.pop(c, None)
+                
+        return iter(ix_samples)
+
+    def __len__(self):
+        return len(self.data_source)
